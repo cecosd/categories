3c5e14568609c8ff0e79d702c37aa60a5c5a188c
Tsvetan <tsvetan.y.petrov@gmail.com>
2019-01-22 22:33:47 +0200

    Added store method in categories controller

--- src/CategoriesController.php
+++ src/CategoriesController.php
@@ -6,11 +6,26 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 use App\Http\Controllers\Controller;
+use Cecos\Category\Category;
 
 class CategoriesController extends Controller
 {
+    protected $category;
+    
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+    
     public function index()
     {
-        return view('categories::index');
+        return view('categories::categories.index', [
+            'items' => Category::all()
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->category->setItem($request);
+        return redirect()->route('categories');
     }
 }
